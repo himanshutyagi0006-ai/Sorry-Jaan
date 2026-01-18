1c72490e43f0b00543fe633fac77b5c4aa3e4f18
himanshutyagi0006-ai <himanshutyagi0006@gmail.com>
2026-01-18 19:40:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function showLove() {
+    const response = document.getElementById("response");
+    response.innerHTML = "You just made my heart complete ❤️ Forever Us 💕";
+    response.style.animation = "pulse 1.5s infinite";
+}
